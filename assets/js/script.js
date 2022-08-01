10895fe2da4c0c05d64a825fb937c94dce0e72fc
ShekChun Lee <leeshekchun@gmail.com>
2022-08-01 05:54:37 -0600

    19.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,38 +10,6 @@ $(document).ready(function() {
   
   }
   
-  function createEl(htmlString, attrs, ...children) {
-    if (typeof htmlString !== "string") {
-      throw Error("Argument 'htmlString' is required and must be a string");
-    }
-  
-    const el = document.createElement(htmlString);
-  
-    if (typeof attrs === "object") {
-      for (let key in attrs) {
-        if (key.substring(0, 2) === "on") {
-          el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
-        } else {
-          el.setAttribute(key, attrs[key]);
-        }
-      }
-    }
-  
-    children.forEach(function(child) {
-      let node;
-  
-      if (child.constructor.name.includes("Element")) {
-        node = child;
-      } else {
-        node = document.createTextNode(child);
-      }
-  
-      el.appendChild(node);
-    });
-  
-    return el;
-  }
-
   // First image is hard coded in index.html
   const carouselSlides = [
     {
@@ -59,6 +27,27 @@ $(document).ready(function() {
       btnUrl: "tickets.html"
     },
   ]
+  
+  carouselSlides.forEach((slide, i) => {
+    $(".carousel-inner").append(`
+  <div class="carousel-item fullscreen-carousel" style="background-image: url("${slide.img}")">
+    <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+              <h2 class="display-4 mb-2">${slide.title}</h2>
+          </div>
+          <div class="row align-items-center justify-content-center"> 
+            <h3>${slide.subtitle}</h3>
+          </div>
+          <div class=" mt-4 row align-items-center justify-content-center"> 
+            <a class="btn btn-primary" href="${slide.btnUrl}">
+                ${slide.btnText}
+            </a>
+          </div>
+        </div>
+    </div>
+  </div>`)
+  })
 
   function dateConverter(UNIX_timestamp){
     const a = new Date(UNIX_timestamp);
